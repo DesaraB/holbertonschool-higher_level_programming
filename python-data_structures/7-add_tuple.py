2fe39db43e4fe70a7028fa9eb4f99af4c93ad381
DesaraB <desarabaroti99@gmail.com>
2025-01-21 09:45:07 +0000

    add tuple

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
 
 def add_tuple(tuple_a=(), tuple_b=()):
-    
+    new_tuple = ()
+    first_tuple = tuple_a + (0, 0)
+    sec_tuple = tuple_b + (0, 0)
+    new_tuple = (first_tuple[0] + sec_tuple[0], first_tuple[1] + sec_tuple[1])
+    return new_tuple
