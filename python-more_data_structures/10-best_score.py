9402d3feb5c4bec8101cefa19a24a368a603276b
DesaraB <desarabaroti99@gmail.com>
2025-01-22 12:22:55 +0000

    best score

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -1,14 +1,7 @@
 #!/usr/bin/python3
 
 def best_score(a_dictionary):
-    if a_dictionary is None:
+    if not a_dictionary:
         return None
-
-    max_score = 0
-    max_key = 0
-
-    for key, value in a_dictionary.items():
-        if value > max_score:
-            max_score = value
-            max_key = key
-    return max_key
+    else:
+        return max(a_dictionary, key=a_dictionary.get)
