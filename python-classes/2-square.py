a4e83093a4f2c494dc5d599470b3f994e640c71c
DesaraB <desarabaroti99@gmail.com>
2025-01-28 11:55:54 +0000

    added new file

---
+++ python-classes/2-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+
+"""module about a class in python"""
+
+
+class Square:
+    """the class Square"""
+    def __init__(self, size=0):
+        """constructor of the object"""
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
