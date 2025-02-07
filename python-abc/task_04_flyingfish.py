619b70d1d256255bdfd369098bcb92a13a511e31
DesaraB <desarabaroti99@gmail.com>
2025-02-07 11:14:10 +0000

    flying fish

--- python-abc/task_04_flyingfish.py
+++ python-abc/task_04_flyingfish.py
@@ -4,20 +4,28 @@
 """module that contains class"""
 
 
-class CountedIterator:
-    def __init__(self, iterable):
-        self.iterable = iter(iterable)
-        self.count = 0
+class Fish:
+    def swim(self):
+        print("The fish is swimming")
 
-    def __next__(self):
-        try:
-            next_item = iter(iterable)
-            self.count += 1
-            return next_item
-        except StopIteration:
-            raise 
-    
-    def get_count(self):
-        return self.count
+    def habitat(self):
+        print("The fish lives in water")
 
 
+class Bird:
+    def fly(self):
+        print("The bird is flying")
+
+    def habitat(self):
+        print("The bird lives in the sky")
+
+
+class FlyingFish(Fish, Bird):
+    def fly(self):
+        print("The flying fish is soaring!")
+
+    def swim(self):
+        print("The flying fish is swimming!")
+
+    def habitat(self):
+        print("The flying fish lives both in water and the sky!")
