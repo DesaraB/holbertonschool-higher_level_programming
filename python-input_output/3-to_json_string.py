1340dfbb19f9452aa3d69f5050121e8a372bea08
DesaraB <desarabaroti99@gmail.com>
2025-02-11 11:48:32 +0000

    json str

---
+++ python-input_output/3-to_json_string.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+import json
+
+"""Module that returns the JSON representation of an obj str"""
+
+
+def to_json_string(my_obj):
+    """func that converts obj to json str"""
+    return json.dumps(my_obj)
