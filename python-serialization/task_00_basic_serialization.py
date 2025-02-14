bb82cb5deea8b4bf8199b0ed87e24efc6e031684
DesaraB <desarabaroti99@gmail.com>
2025-02-14 10:05:52 +0000

    serialization

--- python-serialization/task_00_basic_serialization.py
+++ python-serialization/task_00_basic_serialization.py
@@ -1,41 +1,18 @@
-#!/usr/bin/env python3
-
+#!/usr/bin/python3
 import json
 
 
 def serialize_and_save_to_file(data, filename):
-    """
-    Serializes a Python dictionary and saves it to a JSON file.
-    """
-    with open(filename, 'w', encoding='utf-8') as file:
-        json.dump(data, file, indent=4)
+    """Serialize a Python dictionary to a JSON file"""
+    with open(filename, 'w') as file:
+        json.dump(data, file)
+    print(f"Data serialized and saved to '{filename}'.")
 
 
 def load_and_deserialize(filename):
-
-    """Loads JSON data from a file and
-    deserializes it into a Python dictionary."""
-
-    with open(filename, 'r', encoding='utf-8') as file:
-        return json.load(file)
-
-
-if __name__ == '__main__':
-    data_to_serialize = {
-        "name": "John Doe",
-        "age": 30,
-        "city": "New York"
-    }
-
-# Serialize the data to JSON and save it to a file
-serialize_and_save_to_file(data_to_serialize, 'data.json')
-
-# Output: The data has been serialized and saved to 'data.json'
-print("Data serialized and saved to 'data.json'.")
-
-# Load and deserialize data from 'data.json'
-deserialized_data = load_and_deserialize('data.json')
-
-# Output: The deserialized data
-print("Deserialized Data:")
-print(deserialized_data)
+    """
+    Load and deserialize a JSON file to a Python dictionary.
+    """
+    with open(filename, 'r') as file:
+        data = json.load(file)
+    return data
