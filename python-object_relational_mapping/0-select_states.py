0d80998350dd7ea8300ad9c2b01d7526eb28f770
DesaraB <desarabaroti99@gmail.com>
2025-03-21 13:50:34 +0100

    added file

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -12,9 +12,7 @@ if __name__ == '__main__':
     conn = MySQLdb.connect(host='localhost', port=3306, user=argv[1],
                            passwd=argv[2], db=argv[3])
     cursor = conn.cursor()
-    cursor.execute('SELECT * FROM states\
-                    WHERE states.name LIKE BINARY "N%"\
-                    ORDER BY states.id ASC')
+    cursor.execute('SELECT * FROM states ORDER BY id ASC')
     all_rows = cursor.fetchall()
 
     for row in all_rows:
@@ -22,4 +20,3 @@ if __name__ == '__main__':
 
     cursor.close()
     conn.close()
-    
